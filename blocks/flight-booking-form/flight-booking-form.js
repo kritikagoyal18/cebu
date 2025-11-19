db830229e07d898e97b9e8789149840780a7adb3
Kritika Goyal <kritikagoyal@adobe.com>
2025-11-19 09:46:02 +0530

    Minor functional fixes

--- blocks/flight-booking-form/flight-booking-form.js
+++ blocks/flight-booking-form/flight-booking-form.js
@@ -23,20 +23,20 @@ export default function decorate(block) {
           <span>Flight</span>
         </div>
         <span class="fbf-label">From</span>
-        <div class="fbf-value">Manila MNL</div>
+        <input class="fbf-input fbf-input-from" name="from" type="text" value="Manila MNL" aria-label="From">
       </div>
       <div class="fbf-field">
         <span class="fbf-label">To</span>
-        <div class="fbf-value fbf-dim">Melbourne MEL</div>
+        <input class="fbf-input fbf-input-to" name="to" type="text" value="Melbourne MEL" aria-label="To">
       </div>
       <div class="fbf-field fbf-depart-field">
         <span class="fbf-trip-type">Round-trip</span>
         <span class="fbf-label">Depart</span>
-        <div class="fbf-value">13 January 2026</div>
+        <input class="fbf-input fbf-input-depart" name="depart" type="date" value="2026-01-13" aria-label="Depart">
       </div>
       <div class="fbf-field">
         <span class="fbf-label">Return</span>
-        <div class="fbf-value fbf-dim">23 January 2026</div>
+        <input class="fbf-input fbf-input-return" name="return" type="date" value="2026-01-23" aria-label="Return">
       </div>
       <div class="fbf-actions">
         <a href="#" class="fbf-search-button" aria-label="Search flights">Search flights</a>
@@ -51,6 +51,27 @@ export default function decorate(block) {
     // Navigate to Flight Details page on click
     button.setAttribute('href', 'https://main--cebu--kritikagoyal18.aem.live/en/flight-details');
   }
+
+  // Expose a small API so other scripts can set/read values easily
+  const api = {
+    setValues: ({ from, to, depart, returnDate } = {}) => {
+      if (from !== undefined) block.querySelector('.fbf-input-from').value = from;
+      if (to !== undefined) block.querySelector('.fbf-input-to').value = to;
+      if (depart !== undefined) block.querySelector('.fbf-input-depart').value = depart;
+      if (returnDate !== undefined) block.querySelector('.fbf-input-return').value = returnDate;
+    },
+    getValues: () => ({
+      from: block.querySelector('.fbf-input-from').value,
+      to: block.querySelector('.fbf-input-to').value,
+      depart: block.querySelector('.fbf-input-depart').value,
+      return: block.querySelector('.fbf-input-return').value,
+    }),
+  };
+  // attach to the block and a global namespace for convenience
+  // usage: document.querySelector('.flight-booking-form').fbf.setValues({ from: 'MNL' })
+  // or: window.flightBookingForm.setValues({...})
+  block.fbf = api;
+  window.flightBookingForm = api;
 }
 
 
